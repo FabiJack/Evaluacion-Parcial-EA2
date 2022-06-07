003c88a14b7521d7a4b1fa7d0732db5ba2f9b7f3
Your Name <you@example.com>
2022-06-07 00:10:15 -0400

    COMMIT 2

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ function getBasePath(service) {
     return "https://chilealerta.com/api/query/?user=demo&select=" + service;
 }
 
-function createRowDisaster(reference, magnitude, latitude, longitude, local_time) {
-    return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + local_time + '</td></tr>';
+function createRowDisaster(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td>' + dato5 + '</td></tr>';
 }
 
-function createRowDisaster2(post_title, alert_date, alert_region, state) {
-    return '<tr><td>' + post_title + '</td><td>' + alert_date + '</td><td>' + alert_region + '</td><td>' + state + '</td></tr>';
+function createRowDisasterUrl(dato1, dato2, dato3, dato4, dato5) {
+    return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td><a href=' + dato5 + '>Mas Info</a></td></tr>';
+}
+
+function callInfo(dato1) {
+    return dato1;
 }
