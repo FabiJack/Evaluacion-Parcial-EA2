0660e695fc4c83f5bfef3e989678359632dce23d
Your Name <you@example.com>
2022-06-09 21:00:01 -0400

    COMMIT 2

--- js/script.js
+++ js/script.js
@@ -9,7 +9,3 @@ function createRowDisaster(dato1, dato2, dato3, dato4, dato5) {
 function createRowDisasterUrl(dato1, dato2, dato3, dato4, dato5) {
     return '<tr><td>' + dato1 + '</td><td>' + dato2 + '</td><td>' + dato3 + '</td><td>' + dato4 + '</td><td><a href=' + dato5 + '>Mas Info</a></td></tr>';
 }
-
-function callInfo(dato1) {
-    return dato1;
-}
